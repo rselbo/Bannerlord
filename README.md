7c8f1313e387ad1124669a3c0cb1706c14609c6a
Roar Selbo <github@selbo.net>
2022-06-25 14:59:44 +0200

    Code for my Bannerlord mods

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Bannerlord
+
+A small collection of cheaty/QoL changes
+
+## FoodAutoPurchaser
+
+Automatically buys up to 20 food of each type available if you have more than 20000 gold when entering a town or village.
+
+## Recruiter
+
+Allows recruiting elite troops in towns for 10x their normal cost.
+
+## Refining Mod
+
+Removes smithing stamina cost and allows everybody to refine metals. It also adds a bunch of shortcut refining options so crude iron can be directly smelted into taskamene steel for charcoal cost equal to each normal step.
+
+## Settlement Gold
+
+Increases the amount of gold each town has available for trading.
---
+++ SettlementGoldMod/SettlementGoldMod/GoldModModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace SettlementGoldMod
+{
+    internal class GoldModModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (!(game.GameType is Campaign))
+                return;
+            this.ReplaceModel<DefaultSettlementEconomyModel, GoldSettlementEconomyModel>(gameStarterObject);
+        }
+
+        protected void ReplaceModel<TBaseType, TChildType>(IGameStarter gameStarterObject)
+          where TBaseType : GameModel
+          where TChildType : TBaseType
+        {
+            if (!(gameStarterObject.Models is IList<GameModel> models))
+                return;
+            bool flag = false;
+            for (int index = 0; index < ((ICollection<GameModel>)models).Count; ++index)
+            {
+                if (models[index] is TBaseType)
+                {
+                    flag = true;
+                    if (!(models[index] is TChildType))
+                        models[index] = (GameModel)(object)Activator.CreateInstance<TChildType>();
+                }
+            }
+            if (flag)
+                return;
+            gameStarterObject.AddModel((GameModel)(object)Activator.CreateInstance<TChildType>());
+        }
+    }
+}
