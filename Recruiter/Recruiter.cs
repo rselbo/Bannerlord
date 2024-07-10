f97a4e09d10401b7a877a655d45437fc3f72d9d3
Roar Selbo <github@selbo.net>
2024-07-10 14:55:51 +0200

    Added settings menu and allowed rebuilding the menus when settings change

--- Recruiter/Recruiter.cs
+++ Recruiter/Recruiter.cs
@@ -8,150 +8,205 @@ using TaleWorlds.CampaignSystem.GameMenus;
 using TaleWorlds.CampaignSystem.Overlay;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
-
-// 1.7.*
-//using TaleWorlds.CampaignSystem.SandBox.GameComponents.Party;
-//1.8.*
 using TaleWorlds.CampaignSystem.GameComponents;
 using TaleWorlds.CampaignSystem.Party;
+using System.Runtime;
+using MCM.Abstractions.FluentBuilder;
+using MCM.Common;
+using MCM.Abstractions.Base.Global;
 
 namespace Recruiter
 {
-	public class Recruiter : MBSubModuleBase
-	{
-		int MaxLevelRecruitable = 100;
-		int LevelRecruitablePerClanRank = 2;
-		int ClanRankForElite = 2;
-		bool AllowRecruitElite = true;
+    public class Recruiter : MBSubModuleBase
+    {
+        bool AllowRecruitElite = true;
+        private bool hasAddedMenuToTown = false;
 
-		protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
-		{
- 		}
+        public static RecruiterSettings Settings { get; private set; }
 
-		private void getEliteTroops(CharacterObject troop, List<CharacterObject> list)
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
         {
-			foreach (var upgraded in troop.UpgradeTargets)
-			{
-				if (upgraded.UpgradeTargets.Count() == 0)
-				{
-					list.Add(upgraded);
-				}
-				else
-				{
-					getEliteTroops(upgraded, list);
-				}
-			}
-		}
+            Settings = GlobalSettings<RecruiterSettings>.Instance;
+            Settings.recruiter = this;
+        }
+        protected override void OnSubModuleLoad()
+        {
+            Settings = new RecruiterSettings();
+            base.OnSubModuleLoad();
+        }
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+        }
 
-		public override void OnAfterGameInitializationFinished(Game game, object starterObject)
-		{
-			if (game.GameType is Campaign)
-			{
-				CampaignGameStarter campaignGameStarter = (CampaignGameStarter)starterObject;
-				try
-				{
-					GameMenuOption.OnConditionDelegate hireRecruitsDelegate = delegate (MenuCallbackArgs args)
-					{
-						args.optionLeaveType = GameMenuOption.LeaveType.Recruit;
-						return true;
-					};
-					GameMenuOption.OnConsequenceDelegate hireRecruitsConsequenceDelegate = delegate (MenuCallbackArgs args)
-					{
-						GameMenu.SwitchToMenu("recruit_menu_" + Hero.MainHero.CurrentSettlement.Culture.ToString());
-					};
+        private void getEliteTroops(CharacterObject troop, List<CharacterObject> list)
+        {
+            foreach (var upgraded in troop.UpgradeTargets)
+            {
+                if (upgraded.UpgradeTargets.Count() == 0)
+                {
+                    list.Add(upgraded);
+                }
+                else
+                {
+                    getEliteTroops(upgraded, list);
+                }
+            }
+        }
 
-					int clanTier = Hero.MainHero.Clan.Tier;
-					
-					campaignGameStarter.AddGameMenuOption("town", "recruiter_hire_recruits", "Hire some recruits", hireRecruitsDelegate, hireRecruitsConsequenceDelegate, false, 7, false);
+        private void addTroopsMenu(CampaignGameStarter campaignGameStarter, string menu, CharacterObject troop, bool isElite)
+        {
+            if (isElite && troop.Tier > Settings.MaxRankElite) return;
+            if (isElite ==false && troop.Tier > Settings.MaxRankRegular) return;
 
-                    campaignGameStarter.AddGameMenu("recruit_select_culture", "Select the culture to hire from.", null, GameOverlays.MenuOverlayType.None, GameMenu.MenuFlags.None, null);
-                    //campaignGameStarter.AddGameMenu("recruit_select_troop", "Select the troop type.", null, GameOverlays.MenuOverlayType.None, GameMenu.MenuFlags.None, null);
+            string troopMenu = String.Format("{0}_{1}", menu, troop.ToString().Replace(" ", ""));
 
-                    DefaultPartyWageModel wageModel = new DefaultPartyWageModel();
-					Func<CharacterObject, int, int> getRecruitmentCost = (troop, amount) => {
-						return wageModel.GetTroopRecruitmentCost(troop, Hero.MainHero) * 1 * amount;
-					};
-					Action<string, CharacterObject, int, int> addTroopOption = (menuName, troop, amount, requiredTier) =>
-					{
-						campaignGameStarter.AddGameMenuOption(menuName,
-							String.Format("recruit_{0}_{1}", amount, troop.ToString().Replace(" ", "")),
-							String.Format("Recruit {0} {1} for {2}", amount, troop.ToString(), getRecruitmentCost(troop, amount)),
-							(x) =>
-							{
-								x.optionLeaveType = GameMenuOption.LeaveType.Recruit;
-								return Hero.MainHero.Gold >= getRecruitmentCost(troop, amount) && Hero.MainHero.Clan.Tier >= requiredTier;
-							},
-							(x) =>
-							{
-								MobileParty.MainParty.AddElementToMemberRoster(troop, amount);
-								GiveGoldAction.ApplyForCharacterToSettlement(Hero.MainHero, MobileParty.MainParty.CurrentSettlement, getRecruitmentCost(troop, amount));
-							},
-							false, -1, true);
-					};
-					Action<string, CharacterObject, CharacterObject, List<CharacterObject>> addOptions = (menu, basicTroop, basicEliteTroop, eliteTroops) =>
-					{
-						addTroopOption(menu, basicTroop, 1, 0);
-						addTroopOption(menu, basicEliteTroop, 1, 2);
-						foreach (var eliteTroop in eliteTroops) { addTroopOption(menu, eliteTroop, 1, 4); }
+            campaignGameStarter.AddGameMenu(troopMenu, "How many troops do you want to hire", null, GameOverlays.MenuOverlayType.None, GameMenu.MenuFlags.None, this);
 
-						addTroopOption(menu, basicTroop, 10, 0);
-						addTroopOption(menu, basicEliteTroop, 10, 2);
-						foreach (var eliteTroop in eliteTroops) { addTroopOption(menu, eliteTroop, 10, 4); }
+            foreach (var t in troop.UpgradeTargets)
+            {
+                addTroopsMenu(campaignGameStarter, menu, t, isElite);
+            }
 
-						addTroopOption(menu, basicTroop, 100, 0);
-						addTroopOption(menu, basicEliteTroop, 100, 2);
-						foreach (var eliteTroop in eliteTroops) { addTroopOption(menu, eliteTroop, 100, 4); }
-					};
-
-					foreach (var kingdom in Kingdom.All.DistinctBy(k => k.Culture))
-					{
-						CultureObject culture = kingdom.Culture;
-
-						string menu = "recruit_menu_" + culture.ToString();
-						campaignGameStarter.AddGameMenu(menu, "How many recruits do you want to hire.", null, GameOverlays.MenuOverlayType.None, GameMenu.MenuFlags.None, null);
-
-						List<CharacterObject> eliteTroops = new List<CharacterObject>();
-						getEliteTroops(culture.EliteBasicTroop, eliteTroops);
-
-						addOptions(menu, culture.BasicTroop, culture.EliteBasicTroop, eliteTroops);
-
-						//culture selection
-						campaignGameStarter.AddGameMenuOption(menu, "recruit_select_culture", "Select culture", hireRecruitsDelegate, (x) => GameMenu.SwitchToMenu("recruit_select_culture"), true);
-						campaignGameStarter.AddGameMenuOption("recruit_select_culture", "recruiter_select_" + culture.ToString(), culture.ToString(), hireRecruitsDelegate, (x) => GameMenu.SwitchToMenu("recruit_menu_" + culture.ToString()), true);
-
-						//back
-						campaignGameStarter.AddGameMenuOption(menu, "recruit_back", "Leave", (x) => { x.optionLeaveType = GameMenuOption.LeaveType.Leave; return true; }, (x) => GameMenu.SwitchToMenu("town"), true);
-					}
-
-					CharacterObject basicCustom = Game.Current.ObjectManager.GetObject<CharacterObject>("_basic_root");
-					CharacterObject eliteBasicCustom = Game.Current.ObjectManager.GetObject<CharacterObject>("_elite_root");
-					if (basicCustom != null && eliteBasicCustom != null)
+            campaignGameStarter.AddGameMenuOption(menu, troopMenu, troop.ToString(),
+                (x) =>
+                {
+                    x.optionLeaveType = GameMenuOption.LeaveType.Recruit;
+                    if(isElite)
                     {
-                        List<CharacterObject> eliteTroops = new List<CharacterObject>();
-                        getEliteTroops(eliteBasicCustom, eliteTroops);
+                        int clanEliteTier = Hero.MainHero.Clan.Tier - Settings.ClanRankForElite;
+                        return clanEliteTier >= 0 && Settings.MaxRankElite <= troop.Tier &&
+                               ((Settings.TierRecruitablePerClanRankElite == 0 && troop.Tier == 2) || troop.Tier <= ((clanEliteTier * Settings.TierRecruitablePerClanRankElite) + 1)) &&
+                               Hero.MainHero.Gold >= getRecruitmentCost(troop, 1, isElite);
+                    }
+                    else
+                    {
+                        int clanEliteTier = Hero.MainHero.Clan.Tier - Settings.ClanRankForRegular;
+                        return clanEliteTier >= 0 && Settings.MaxRankRegular <= troop.Tier && 
+                               troop.Tier <= ((Hero.MainHero.Clan.Tier * Settings.TierRecruitablePerClanRank) + 1) &&
+                               Hero.MainHero.Gold >= getRecruitmentCost(troop, 1, isElite);
+                    }
+                },
+                (x) =>
+                {
+                    GameMenu.SwitchToMenu(troopMenu);
+                }, relatedObject: this);
+
+            Action<string, int> addTroopOption = (menuName, amount) =>
+            {
+                campaignGameStarter.AddGameMenuOption(menuName,
+                    String.Format("{0}_{1}", menuName, amount),
+                    String.Format("Recruit {0} {1} for {2}", amount, troop.ToString(), getRecruitmentCost(troop, amount, isElite)),
+                    (x) =>
+                    {
+                        x.optionLeaveType = GameMenuOption.LeaveType.Recruit;
+                        return Hero.MainHero.Gold >= getRecruitmentCost(troop, amount, isElite);
+                    },
+                    (x) =>
+                    {
+                        MobileParty.MainParty.AddElementToMemberRoster(troop, amount);
+                        GiveGoldAction.ApplyForCharacterToSettlement(Hero.MainHero, MobileParty.MainParty.CurrentSettlement, getRecruitmentCost(troop, amount, isElite));
+                    },
+                    false, -1, true, this);
+            };
+
+            addTroopOption(troopMenu, 1);
+            addTroopOption(troopMenu, 10);
+            addTroopOption(troopMenu, 100);
+            campaignGameStarter.AddGameMenuOption(troopMenu, "recruit_back", "Leave", (x) => { x.optionLeaveType = GameMenuOption.LeaveType.Leave; return true; }, (x) => GameMenu.SwitchToMenu(menu), true, relatedObject: this);
+        }
+
+        private int getRecruitmentCost(CharacterObject troop, int amount, bool isElite)
+        {
+            DefaultPartyWageModel wageModel = new DefaultPartyWageModel();
+            float multiplier = isElite ? Settings.CostMultiplierElite : Settings.CostMultiplierRegular;
+            return (int)(wageModel.GetTroopRecruitmentCost(troop, Hero.MainHero) * multiplier * amount);
+        }
+
+        public override void OnAfterGameInitializationFinished(Game game, object starterObject)
+        {
+            RebuildMenu(game, (CampaignGameStarter)starterObject);
+        }
+        public void RebuildMenu(Game game, CampaignGameStarter starterObject)
+        {
+            if (game.GameType is Campaign)
+            {
+                CampaignGameStarter campaignGameStarter = (CampaignGameStarter)starterObject;
+                Campaign campaign = (Campaign)game.GameType;
+                campaign.GameMenuManager.RemoveRelatedGameMenus(this);
+                campaign.GameMenuManager.RemoveRelatedGameMenuOptions(this);
+                try
+                {
+                    GameMenuOption.OnConditionDelegate hireRecruitsDelegate = delegate (MenuCallbackArgs args)
+                    {
+                        args.optionLeaveType = GameMenuOption.LeaveType.Recruit;
+                        return true;
+                    };
+                    GameMenuOption.OnConsequenceDelegate hireRecruitsConsequenceDelegate = delegate (MenuCallbackArgs args)
+                    {
+                        GameMenu.SwitchToMenu("recruit_menu_" + Hero.MainHero.CurrentSettlement.Culture.ToString());
+                    };
+
+                    int clanTier = Hero.MainHero.Clan.Tier;
+
+                    if (!hasAddedMenuToTown)
+                    {
+                        //hasAddedMenuToTown = true;
+                        campaignGameStarter.AddGameMenuOption("town", "recruiter_hire_recruits", "Hire some recruits", hireRecruitsDelegate, hireRecruitsConsequenceDelegate, false, 7, false, this);
+                        campaignGameStarter.AddGameMenuOption("recruit_select_culture", "recruiter_back", "Leave", (x) => { x.optionLeaveType = GameMenuOption.LeaveType.Leave; return true; }, (x) => GameMenu.SwitchToMenu("town"), true, relatedObject: this);
+                    }
+
+                    campaignGameStarter.AddGameMenu("recruit_select_culture", "Select the culture to hire from.", null, GameOverlays.MenuOverlayType.None, GameMenu.MenuFlags.None, this);
+
+                    foreach (var kingdom in Kingdom.All.DistinctBy(k => k.Culture))
+                    {
+                        CultureObject culture = kingdom.Culture;
+
+                        string menu = "recruit_menu_" + culture.ToString();
+                        campaignGameStarter.AddGameMenu(menu, "Select troop type to recruit.", null, GameOverlays.MenuOverlayType.None, GameMenu.MenuFlags.None, this);
+
+                        if (AllowRecruitElite) addTroopsMenu(campaignGameStarter, menu, culture.EliteBasicTroop, true);
+                        addTroopsMenu(campaignGameStarter, menu, culture.BasicTroop, false);
+
+                        //culture selection
+                        campaignGameStarter.AddGameMenuOption(menu, "recruit_select_culture", "Select culture", hireRecruitsDelegate, (x) => GameMenu.SwitchToMenu("recruit_select_culture"), true, relatedObject: this);
+                        campaignGameStarter.AddGameMenuOption("recruit_select_culture", "recruiter_select_" + culture.ToString(), culture.ToString(), hireRecruitsDelegate, (x) => GameMenu.SwitchToMenu("recruit_menu_" + culture.ToString()), true, relatedObject: this);
+
+                        //back
+                        campaignGameStarter.AddGameMenuOption(menu, "recruit_back", "Leave", (x) => { x.optionLeaveType = GameMenuOption.LeaveType.Leave; return true; }, (x) => GameMenu.SwitchToMenu("town"), true, relatedObject: this);
+                    }
+
+                    CharacterObject basicCustom = Game.Current.ObjectManager.GetObject<CharacterObject>("_basic_root");
+                    CharacterObject eliteBasicCustom = Game.Current.ObjectManager.GetObject<CharacterObject>("_elite_root");
+                    if (basicCustom != null && eliteBasicCustom != null)
+                    {
                         string menu = "recruit_menu_custom";
-						campaignGameStarter.AddGameMenu(menu, "How many recruits do you want to hire.", null, GameOverlays.MenuOverlayType.None, GameMenu.MenuFlags.None, null);
-						addOptions(menu, basicCustom, eliteBasicCustom, eliteTroops);
+                        campaignGameStarter.AddGameMenu(menu, "How many recruits do you want to hire.", null, GameOverlays.MenuOverlayType.None, GameMenu.MenuFlags.None, this);
 
-						//culture selection
-						campaignGameStarter.AddGameMenuOption(menu, "recruit_select_culture", "Select culture", hireRecruitsDelegate, (x) => GameMenu.SwitchToMenu("recruit_select_culture"), true);
-						campaignGameStarter.AddGameMenuOption("recruit_select_culture", "recruiter_select_custom", "Custom", hireRecruitsDelegate, (x) => GameMenu.SwitchToMenu("recruit_menu_custom"), true);
+                        if (AllowRecruitElite) addTroopsMenu(campaignGameStarter, menu, eliteBasicCustom, true);
+                        addTroopsMenu(campaignGameStarter, menu, basicCustom, false);
 
-						//back
-						campaignGameStarter.AddGameMenuOption(menu, "recruit_back", "Leave", (x) => { x.optionLeaveType = GameMenuOption.LeaveType.Leave; return true; }, (x) => GameMenu.SwitchToMenu("town"), true);
-					}
-					//List<CharacterObject> eliteTroops = new List<CharacterObject>();
-					//getEliteTroops(eliteBasicCustom, eliteTroops);
-				}
-				catch (Exception ex)
-				{
-					MessageBox.Show(ex.ToString());
-				}
-				campaignGameStarter.AddGameMenuOption("recruit_select_culture", "recruiter_back", "Leave", (x) => { x.optionLeaveType = GameMenuOption.LeaveType.Leave; return true; }, (x) => GameMenu.SwitchToMenu("town"), true);
+                        //culture selection
+                        campaignGameStarter.AddGameMenuOption(menu, "recruit_select_culture", "Select culture", hireRecruitsDelegate, (x) => GameMenu.SwitchToMenu("recruit_select_culture"), true, relatedObject: this);
+                        campaignGameStarter.AddGameMenuOption("recruit_select_culture", "recruiter_select_custom", "Custom", hireRecruitsDelegate, (x) => GameMenu.SwitchToMenu("recruit_menu_custom"), true, relatedObject: this);
 
-			}
-		}
+                        //back
+                        campaignGameStarter.AddGameMenuOption(menu, "recruit_back", "Leave", (x) => { x.optionLeaveType = GameMenuOption.LeaveType.Leave; return true; }, (x) => GameMenu.SwitchToMenu("town"), true, relatedObject: this);
+                    }
+                    //List<CharacterObject> eliteTroops = new List<CharacterObject>();
+                    //getEliteTroops(eliteBasicCustom, eliteTroops);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
 
+            }
+        }
 
-	}
+        public void RebuildMenu()
+        {
+            if(Game.Current != null && SandBoxManager.Instance != null && SandBoxManager.Instance.GameStarter != null)
+                RebuildMenu(Game.Current, SandBoxManager.Instance.GameStarter);
+        }
+    }
 }
---
+++ Recruiter/RecruiterSettings.cs
@@ -0,0 +1,57 @@
+﻿using MCM.Abstractions.Attributes;
+using MCM.Abstractions.Attributes.v2;
+using MCM.Abstractions.Base.Global;
+using System.Runtime.CompilerServices;
+
+namespace Recruiter
+{
+    public class RecruiterSettings : AttributeGlobalSettings<RecruiterSettings>
+    {
+        public Recruiter recruiter = null;
+        public override string Id => "Recruiter";
+
+        public override string DisplayName => "Recruiter";
+
+        public override string FolderName => "Recruiter";
+
+        public override string FormatType => "json";
+
+        [SettingPropertyInteger("Clan rank for regular", 0, 6, "0", RequireRestart = false, HintText = "Minimum clan rank to hire regular troops", Order = 0)]
+        [SettingPropertyGroup("General", GroupOrder = 0)]
+        public int ClanRankForRegular { get; set; } = 2;
+        [SettingPropertyInteger("Clan rank for elite", 0, 6, "0", RequireRestart = false, HintText = "Minimum clan rank to hire elite troops", Order = 0)]
+        [SettingPropertyGroup("General", GroupOrder = 0)]
+        public int ClanRankForElite { get; set; } = 1;
+
+        [SettingPropertyInteger("Max recruitable tier for regulars", 0, 6, "0", RequireRestart = false, HintText = "Maximum recruitable tier for regular troops", Order = 0)]
+        [SettingPropertyGroup("General", GroupOrder = 0)]
+        public int MaxRankRegular { get; set; } = 1;
+        [SettingPropertyInteger("Max recruitable tier for elite", 0, 6, "0", RequireRestart = false, HintText = "Maximum recruitable tier for elite troops", Order = 0)]
+        [SettingPropertyGroup("General", GroupOrder = 0)]
+        public int MaxRankElite { get; set; } = 1;
+
+        [SettingPropertyInteger("Troop tiers unlocked per Clan rank for regular troops", 0, 6, "0", RequireRestart = false, HintText = "How many tiers of troops are unlocked per Clan rank", Order = 0)]
+        [SettingPropertyGroup("General", GroupOrder = 0)]
+        public int TierRecruitablePerClanRank { get; set; } = 2;
+        [SettingPropertyInteger("Troop tiers unlocked per Clan rank for elite troops", 0, 6, "0", RequireRestart = false, HintText = "How many tiers of troops are unlocked per Clan rank", Order = 0)]
+        [SettingPropertyGroup("General", GroupOrder = 0)]
+        public int TierRecruitablePerClanRankElite { get; set; } = 2;
+        [SettingPropertyFloatingInteger("Troop cost multiplier for regular troops", 0.1f, 20, "0", RequireRestart = false, HintText = "Multiplier for hiring regular troops ", Order = 0)]
+        [SettingPropertyGroup("General", GroupOrder = 0)]
+        public float CostMultiplierRegular { get; set; } = 1.0f;
+        [SettingPropertyFloatingInteger("Troop cost multiplier for elite troops", 0.1f, 20, "0", RequireRestart = false, HintText = "Multiplier for hiring elite troops ", Order = 0)]
+        [SettingPropertyGroup("General", GroupOrder = 0)]
+        public float CostMultiplierElite { get; set; } = 1.0f;
+
+        public override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+            if(recruiter != null)
+            {
+                recruiter.RebuildMenu();
+
+            }
+
+        }
+    }
+}
